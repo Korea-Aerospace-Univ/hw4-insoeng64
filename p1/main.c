537a88a76bface6428a42c5937ce574cac869296
insoeng64 <108975160+insoeng64@users.noreply.github.com>
2026-04-15 16:13:32 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -4,14 +4,14 @@ int main()
 {
 	int correct = 0;
 	int tried = 0;
-	int i = 1;
+	int i = 0;
 
 	scanf("%d", &correct);
 	do {
 		scanf("%d", &i);
 		if (i < correct) printf("%d<?\n", i);
 		else if (i > correct) printf("%d>?\n", i);
-		else if (i == correct) printf("%d==?\n", i);
+		else printf("%d==?\n", i);
         tried++;
 	} while (i != correct);
 	printf("%d", tried);

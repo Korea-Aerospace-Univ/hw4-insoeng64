5cc7e02ef3bcaddb6910a7894f0ebb8b6d698fa5
insoeng64 <108975160+insoeng64@users.noreply.github.com>
2026-04-15 16:22:16 +0900

    Update main.c

--- p2/main.c
+++ p2/main.c
@@ -17,16 +17,18 @@ int main()
             alphaCount++;
             if(alphaCount > maxAlphaCount)
                 maxAlphaCount = alphaCount;
+            numberCount = 0;
         }
-        else 
-            alphaCount = 0;
-        if(c <= '9' && c >= '0') {
+        else if(c <= '9' && c >= '0') {
             numberCount++;
             if(numberCount > maxNumCount)
                 maxNumCount = numberCount;
+            alphaCount = 0;
         }
-        else 
+        else {
+            alphaCount = 0;
             numberCount = 0;
+        }
     }
     printf("%d\n%d", maxAlphaCount, maxNumCount);
 	return 0;
